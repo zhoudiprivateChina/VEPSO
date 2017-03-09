38e0f45d1e334bbdb9b4bf103ed7e94f86f053de
zhoudiprivateChina <85148962@qq.com>
2017-03-09 22:31:38 +0800

    Update Dominates2.m

--- Dominates2.m
+++ Dominates2.m
@@ -1,7 +1,7 @@
 function dom=Dominates2(x,y,numOfObj)
 
     if isstruct(x)
-        x=x.Cost(numOfObj);
+        x=x.Cost(numOfObj); % Are u sure? Why do you just compare the cost on numOfObj-th obj? what about the first or second or other objs?
     end
 
     if isstruct(y)
